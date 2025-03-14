e060cb9591d3ee4eff63af2de234c2bfc2b2c81d
hayato-shino05 <nthdungvn@gmail.com>
2025-03-15 04:37:23 +0700

    big updateupdate

---
+++ src/config/skills.config.ts
@@ -0,0 +1,62 @@
+export const skillsConfig = [
+  {
+    category: "Languages",
+    technologies: [
+      "JavaScript",
+      "TypeScript",
+      "GraphQL",
+      "C",
+      "C++",
+      "C#",
+      "PHP",
+      "Python"
+    ]
+  },
+  {
+    category: "Frameworks",
+    technologies: [
+      "Next.js",
+      "React Native",
+      "Redux",
+      "Redux Toolkit",
+      "Nest.js",
+      "NodeJS",
+      "Bootstrap",
+      "Flask"
+    ]
+  },
+  {
+    category: "Styling",
+    technologies: [
+      "Tailwind CSS",
+      "SCSS",
+      "styled-components",
+      "CSS",
+      "Headless UI",
+      "HTML"
+    ]
+  },
+  {
+    category: "Database",
+    technologies: [
+      "MySQL",
+      "MongoDB",
+      "PostgreSQL",
+      "Firebase"
+    ]
+  },
+  {
+    category: "Tools",
+    technologies: [
+      "Git",
+      "Jira",
+      "Confluence",
+      "Figma",
+      "VS Code",
+      "Android Studio",
+      "Docker",
+      "Render",
+      "Photoshop"
+    ]
+  }
+];
