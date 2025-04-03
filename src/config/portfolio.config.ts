be3dd427ff46232a3426f284065b7c229ef78565
hayato-shino05 <nthdungvn@gmail.com>
2025-04-03 10:49:31 +0700

    update exp

--- src/config/portfolio.config.ts
+++ src/config/portfolio.config.ts
@@ -9,6 +9,6 @@ export const portfolioConfig: Portfolio = {
     mail: "mailto:hayatoshino05@gmail.com",
     linkedin: "https://linkedin.com/in/hayato-shino05", // Thay thế bằng LinkedIn của bạn nếu có
     phone: "tel:+84xxxxxxxxxx", // Thay thế bằng số điện thoại của bạn
-    facebook: "https://www.facebook.com/hayato-shino05", // Thay thế bằng Facebook của bạn nếu có
+    facebook: "https://www.facebook.com/hayatoshino05", // Thay thế bằng Facebook của bạn nếu có
   }
 };
