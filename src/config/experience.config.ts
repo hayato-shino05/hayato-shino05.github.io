8d199965a9af10c3dcc95a8dc1734d4cf73165ba
Hayato-shino05 <nthdungvn@gmail.com>
2025-04-07 06:19:07 +0700

    Refactor experience entries: update title and descriptions for POD Content Manager role, and remove outdated TIL files while adding new web design and development insights.

--- src/config/experience.config.ts
+++ src/config/experience.config.ts
@@ -2,7 +2,7 @@ import type { Experience } from "@/types";
 
 export const experiencesConfig: Experience[] = [
   {
-    title: "Listing and Upload",
+    title: "POD Content Manager",
     employmentType: "Freelance",
     company: {
       name: "VCOMGROUP",
@@ -14,10 +14,10 @@ export const experiencesConfig: Experience[] = [
     start: "September 2024",
     end: "Present",
     description: [
-      "Listing and Upload products to Teepublic website",
-      "Listing and Upload products to Redbubble website",
-      "Lead the team to upload, training and support products to other websites",
+      "Managed the uploading and listing of products on TeePublic, ensuring high-quality presentation and platform compliance.",
+      "Oversaw content deployment on Redbubble, optimizing product visibility and engagement.",
+      "Led a team in content management, providing training and support to scale product listings across multiple platforms.",
     ],
-    achievement: "",
+    achievement: "Developed a trained team to streamline content operations and enhance productivity in Print-on-Demand workflows.",
   },
 ];
